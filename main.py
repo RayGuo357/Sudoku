ab21ecf03c74c7d4774338b2f00343e990eb1da8
Raymond Guo <rmguo@live.unc.edu>
2021-06-29 15:37:46 -0400

    Unchangable given numbers

--- main.py
+++ main.py
@@ -24,32 +24,38 @@ grid = [
     [0, 0, 8, 0, 0, 0, 0, 7, 4]
 ]
 
-rectGroup = [
-    [0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0]
+defaultNumbers = [
+    [False, False, False, False, False, False, False, False, False],
+    [False, False, False, False, False, False, False, False, False],
+    [False, False, False, False, False, False, False, False, False],
+    [False, False, False, False, False, False, False, False, False],
+    [False, False, False, False, False, False, False, False, False],
+    [False, False, False, False, False, False, False, False, False],
+    [False, False, False, False, False, False, False, False, False],
+    [False, False, False, False, False, False, False, False, False],
+    [False, False, False, False, False, False, False, False, False]
 ]
 
 selectedMouseX = 0
 selectedMouseY = 0
 
+def initBoardNumbers():
+    for i in range(9):
+        for j in range(9):
+            if grid[j][i] != 0:
+                defaultNumbers[j][i] = True
+
 def draw():
     window.blit(pygame.font.SysFont("comicsans", 40).render("Test", 1, (255, 255, 255)), (20, 570)) 
     for i in range(9):
         for j in range(9):
-            if j == selectedMouseY and i == selectedMouseX:
-                rectGroup[j][i] = pygame.draw.rect(window, (0, 0, 0), (i * tileSize, j * tileSize, tileSize, tileSize))
-            else:
-                rectGroup[j][i] = pygame.draw.rect(window, tileColor, (i * tileSize, j * tileSize, tileSize, tileSize))
+            pygame.draw.rect(window, tileColor, (i * tileSize, j * tileSize, tileSize, tileSize))
             
             if grid[j][i] != 0:
-                window.blit(pygame.font.SysFont("TimesRoman", 40). render(str(grid[j][i]), 1, (255, 255, 255)), ((i * tileSize) + adjustX, (j * tileSize) + adjustY))
+                if defaultNumbers[j][i]:
+                    window.blit(pygame.font.SysFont("TimesRoman", 40, True). render(str(grid[j][i]), 1, (255, 255, 255)), ((i * tileSize) + adjustX, (j * tileSize) + adjustY))
+                else:
+                    window.blit(pygame.font.SysFont("TimesRoman", 40). render(str(grid[j][i]), 1, (255, 255, 255)), ((i * tileSize) + adjustX, (j * tileSize) + adjustY))
             
     for i in range(8):
         if (i + 1) % 3 != 0: 
@@ -59,6 +65,7 @@ def draw():
             pygame.draw.line(window, (255, 255, 255), ((i + 1) * tileSize, 0), ((i + 1) * tileSize, 600), 4)
             pygame.draw.line(window, (255, 255, 255), (0, (i + 1) * tileSize), (600, (i + 1) * tileSize), 4)
 
+initBoardNumbers()
 run = True
 
 while run:
@@ -69,15 +76,13 @@ while run:
             selectedMouseX = math.floor(pygame.mouse.get_pos()[0] / tileSize)
             selectedMouseY = math.floor(pygame.mouse.get_pos()[1] / tileSize)
 
-            rectGroup[selectedMouseY][selectedMouseX].inflate(2, 2)
-            grid[selectedMouseY][selectedMouseX] = (grid[selectedMouseY][selectedMouseX] + 1) % 10
-            print("Selected tile: " + "(" + str(selectedMouseX) + ", " + str(selectedMouseY) + ")")
+            if not defaultNumbers[selectedMouseY][selectedMouseX]:
+                grid[selectedMouseY][selectedMouseX] = (grid[selectedMouseY][selectedMouseX] + 1) % 10
+                print("Selected tile: " + "(" + str(selectedMouseX) + ", " + str(selectedMouseY) + ")")
         if e.type == pygame.QUIT:
             run = False
     
     draw()
-
-
     pygame.display.update()
 
 pygame.quit()
